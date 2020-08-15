59aed56a5760af84761309cfb11ce66ce1946621
mvanmeerten <m.c.vanmeerten@student.tudelft.nl>
2020-08-15 14:41:28 +0200

    Highlight original text in different colors depending on category

--- PredictAPI/app.py
+++ PredictAPI/app.py
@@ -4,7 +4,7 @@ import boto3
 from flask import Flask, request, flash, redirect, render_template, send_from_directory, url_for, jsonify
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from tika import parser
+from pdfminer.high_level import extract_text
 from werkzeug.utils import secure_filename
 from wtforms import SubmitField
 
@@ -12,127 +12,100 @@ client = boto3.client(service_name='comprehendmedical', region_name='eu-west-2')
 
 # Init app
 UPLOAD_FOLDER = 'C:/Users/Martijn/PycharmProjects/CiphixHealthCase/PredictAPI/docs'
-ALLOWED_EXTENSIONS = {'txt', 'pdf'}
+ALLOWED_EXTENSIONS = {'pdf'}
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
+    """
+    Only allow files with extensions from ALLOWED_EXTENSIONS
+    :param filename: The filename of the uploaded file
+    :return If allowed, True, else, False
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def parse_entities(entity_list):
-    result = ""
-    print(entity_list)
-    for entity in entity_list:
-        print(type(entity))
-        result += entity['Text'] + ", category: " + entity['Category'] + ', type: ' + entity['Type'] + '\n'
-        if entity['Attributes']:
-            for attribute in entity['Attributes']:
-                result += '- ' + attribute['Text'] + ', type: ' + attribute['Type'] + os.linesep
+def choose_color(category):
+    COLOR_CATEGORIES = {
+        "ANATOMY": "cyan",
+        "MEDICAL_CONDITION": "lightgreen",
+        "MEDICATION": "yellow",
+        "PROTECTED_HEALTH_INFORMATION": "orange",
+        "TEST_TREATMENT_PROCEDURE": "pink"
+    }
 
-    print(result)
-    return result
+    return "<span style=\"background-color:" + COLOR_CATEGORIES[category] + "\">"
+
+
+def highlight_entities(text, entities):
+    """
+    Highlight entities found in original text.
+    :param text: The text to highlight in
+    :param entities: The entities found by the NER system
+    :return: The original text with entities highlighted
+    """
+    last_index = 0
+    for entity in entities:
+        # index throws an exception if entity is not found in the text.
+        try:
+            entity_index = text.index(entity['Text'], last_index)
+            last_index = entity_index
+            # Mark the found entity
+            text = text[:entity_index] + choose_color(entity['Category']) + text[entity_index:entity_index+len(entity['Text'])] + "</span>" + text[entity_index+len(entity['Text']):]
+            # Mark potential attributes as well
+            if 'Attributes' in entity:
+                for attribute in entity['Attributes']:
+                    attribute_index = text.index(attribute['Text'], last_index)
+                    last_index = attribute_index
+                    text = text[:attribute_index] + "<mark>" + text[attribute_index:attribute_index + len(attribute['Text'])] + "</mark>" + text[attribute_index + len(attribute['Text']):]
+        except ValueError:
+            pass
+
+    return text
 
 
 @app.route('/', methods=['get', 'POST'])
 def index():
+    """
+    Load the page.
+    :return:
+    """
     form = UploadForm()
+    # If the user uploaded a file, save, parse, and extract entities from text.
     if request.method == "POST":
         if form.validate_on_submit():
             if 'file' not in request.files:
-                flash('No file part')
                 return redirect(request.url)
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
             if file.filename == '':
-                flash('No selected file')
                 return redirect(request.url)
             if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                file.save(filepath)
-                text = parser.from_file(filepath)['content']
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+                file.save(file_path)
+                text = extract_text(file)
                 result = client.detect_entities(Text=text)
-                # print(jsonify(result['Entities']).content)
+                text = highlight_entities(text, result['Entities'])
+                return render_template('index.html', form=form, entities=result['Entities'], text=text[:-1])
 
-                # return jsonify(result['Entities'])
-                print(type(result['Entities']))
-                # entities = parse_entities(result['Entities'])
-                return render_template('index.html', form=form, entities=result['Entities'], text=text)
-                # return redirect(url_for('uploaded_file', filename=filename))
-    text = 'Pt is 87 yo woman, highschool teacher with past medical history that includes - status post cardiac catheterization in April 2019. She presents today with palpitations and chest pressure. HPI : Sleeping trouble on present dosage of Clonidine. Severe Rash on face and leg, slightly itchy Meds : Vyvanse 50 mgs po at breakfast daily, Clonidine 0.2 mgs -- 1 and 1 / 2 tabs po qhs HEENT : Boggy inferior turbinates, No oropharyngeal lesion Lungs : clear Heart : Regular rhythm Skin : Mild erythematous eruption to hairline Follow-up as scheduled'
-    result = client.detect_entities(Text=text)
-    return render_template('index.html', form=form, entities=result['Entities'], text=text)
-
-
-@app.route('/display/<filename>')
-def display_doc(filename):
-    return redirect(url_for('uploaded_file', filename='docs/' + filename), code=301)
+    return render_template('index.html', form=form)
 
 
 class UploadForm(FlaskForm):
+    """
+    Flask_wtf form for uploading files.
+    """
     file = FileField(validators=[
         FileRequired(),
-        FileAllowed(ALLOWED_EXTENSIONS, 'Only txt and pdf files are allowed.')
+        FileAllowed(ALLOWED_EXTENSIONS, 'Only pdf files are allowed.')
     ])
     submit = SubmitField("submit")
 
 
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#             file.save(filepath)
-#             text = parser.from_file(filepath)['content']
-#             result = client.detect_entities(Text=text)
-#             # return jsonify(result['Entities'])
-#             return result
-#
-#             # return redirect(url_for('uploaded_file',
-#             #                         filename=filename))
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
-#
-#
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-# @app.route('/', methods=['POST'])
-# def extract_entities():
-#     data = request.get_json()
-#     print(data)
-#     # result = client.detect_entities(Text='cerealx 84 mg daily')
-#     result = client.detect_entities(Text=data['text'])
-#     entities = result['Entities']
-#     for entity in entities:
-#         print('Entity', entity)
-#     return jsonify(entities)
-
-
 # Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
